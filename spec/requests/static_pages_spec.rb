f08f450add16e52ea8cf605aa255c65e13f6c773
John Searing <jmsearing@gmail.com>
2014-04-28 23:05:43 -0700

    Basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,7 +20,7 @@ describe "Static pages" do
 	end
 
 	describe "About page" do
-		before {visit about_path }
+		before { visit about_path }
 
 		it { should have_content('About') }
 		it { should have_title(full_title('About')) }
@@ -31,5 +31,20 @@ describe "Static pages" do
 
 		it { should have_content('Contact') }
 		it { should have_title(full_title('Contact')) }
+	end
+
+	it "Should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contact"
+		expect(page).to have_title(full_title('Contact'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign up'))
+		click_link "sample app"
+		expect(page).to have_title(full_title(''))
 	end
 end
